6e85df04a4af555580bf2fed67697d0b64495e7c
aderylo <a.m.derylo@gmail.com>
2022-06-15 13:54:32 +0200

    Implement finding vnode and suspending vnode

--- usr/src/minix/servers/vfs/notify.c
+++ usr/src/minix/servers/vfs/notify.c
@@ -1,5 +1,34 @@
 #include "fs.h"
+#include "vnode.h"
+#include "file.h"
+#include <fcntl.h>
+#include <stdio.h>
 
 int do_notify(void) {
-	return(ENOSYS);  // TODO: implementacja VFS_NOTIFY
+//    struct fproc *caller = fp;
+    struct filp *f;
+    register struct vnode *vp;
+
+    // got that from do_utimens
+    if ((f = get_filp(job_m_in.m_lc_vfs_notify.fd, VNODE_READ)) == NULL)
+        return (err_code);
+    vp = f->filp_vno;        /* get vnode pointer */
+    // there is some not explicitly stated lock on vnode as well as flip
+    // look for unlock_flip(flip)
+
+    printf("dupa\n");
+    printf("%d", job_m_in.m_lc_vfs_notify.event);
+
+    switch (job_m_in.m_lc_vfs_notify.event) {
+        case NOTIFY_OPEN:
+            suspend(FP_BLOCKED_ON_LOCK);
+            break;
+        default:
+            // throw error
+            break;
+    }
+
+
+    return (SUSPEND);
+//	return(ENOSYS);  // TODO: implementacja VFS_NOTIFY
 }
